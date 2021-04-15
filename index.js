ef377bc83b42102e07db84db7a6f526c60db53e2
Júnior Nunes <denisonprobable13@gmai.com>
2021-04-15 11:06:38 -0300

    get method

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use("/meusite", express.static(path.join(__dirname, 'client')));
+
+app.get("/", (req, res) => {
+    res.type("html");
+    res.send("<h1>Hello, world! from GET!</h1>");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
