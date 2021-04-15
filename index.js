462583814b4b25235808b3818245c62a710eb77f
Júnior Nunes <denisonprobable13@gmai.com>
2021-04-15 15:01:39 -0300

    learning about routes

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+const { get } = require('https');
 
 
 const app = express();
 
 app.use("/meusite", express.static(path.join(__dirname, 'client')));
 
-let consoleMethod = (req, res, next) => {
+const consoleMethod = (req, res, next) => {
     console.log(req.body);
     next();
 };
 
-let hello = (req, res) => {
+const hello = (req, res) => {
     res.send("Hello World!")
 };
 
-app.use("/", bodyParser.json());
+const students = [
+    { id: 0, nome: "Jose" },
+    { id: 1, nome: "Maria" },
+    { id: 2, nome: "João" },
+    { id: 3, nome: "Marcos" },
+];
+
+const getStudents = (req, res) => {
+    res.json(JSON.stringify(students));
+}
+
+const getStudent = (req, res) => {
+    console.log(req.query);
+    let student = students[req.query.id];
+    res.json(student);
+};
+
+app.use("/", bodyParser.urlencoded());
 app.use("/", consoleMethod);
+
 app.get("/", hello);
+app.get("/students", getStudents);
+app.get("/student", getStudent);
 
 app.post("/", hello);
 
